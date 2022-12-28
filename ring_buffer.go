go-zookeeper/zk

Resolving deadlocks with watch events, especially after reconnection.

ring_buffer.go
@@ -1,0 +1,134 @@
+package zk
+
+import "fmt"
+
+// RingBuffer is a circular buffer that overwrites the oldest item when full.
+// It is not thread-safe.
+type RingBuffer[T any] struct {
+	// The buffer.
+	buf []T
+	// The index of the first item in the buffer.
+	head int
+	// The index of the last item in the buffer.
+	tail int
+	// The number of items in the buffer.
+	count int
+	// The capacity of the buffer.
+	capacity int
+}
+
+// NewRingBuffer creates a new RingBuffer with the given capacity.
+func NewRingBuffer[T any](capacity int) *RingBuffer[T] {
+	return &RingBuffer[T]{
+		buf:      make([]T, capacity),
+		capacity: capacity,
+	}
+}
+
+func (rb *RingBuffer[T]) IsEmpty() bool {
+	return rb.count == 0
+}
+
+// Len returns the number of items in the buffer.
+func (rb *RingBuffer[T]) Len() int {
+	return rb.count
+}
+
+// Cap returns the capacity of the buffer.
+func (rb *RingBuffer[T]) Cap() int {
+	return rb.capacity
+}
+
+// Offer adds an item to the buffer, if there is space.
+// Returns true if the item was added, false otherwise (buffer full).
+func (rb *RingBuffer[T]) Offer(t T) bool {
+	if rb.count == rb.capacity {
+		return false
+	}
+	rb.buf[rb.tail] = t
+	rb.tail = (rb.tail + 1) % rb.capacity
+	rb.count++
+	return true
+}
+
+// Push adds an item to the buffer.
+// If the buffer is full, the oldest item is overwritten.
+func (rb *RingBuffer[T]) Push(t T) {
+	rb.buf[rb.tail] = t
+	rb.tail = (rb.tail + 1) % rb.capacity
+	if rb.count == rb.capacity {
+		rb.head = rb.tail
+	} else {
+		rb.count++
+	}
+}
+
+// Peek returns the oldest item in the buffer, without removing it.
+// If the buffer is empty, returns the zero value and false.
+func (rb *RingBuffer[T]) Peek() (T, bool) {
+	if rb.count == 0 {
+		var zero T
+		return zero, false
+	}
+	return rb.buf[rb.head], true
+}
+
+// Pop returns the oldest item in the buffer, removing it.
+// If the buffer is empty, returns the zero value and false.
+func (rb *RingBuffer[T]) Pop() (T, bool) {
+	if rb.count == 0 {
+		var zero T
+		return zero, false
+	}
+	t := rb.buf[rb.head]
+	rb.head = (rb.head + 1) % rb.capacity
+	rb.count--
+	return t, true
+}
+
+// Clear removes all items from the buffer.
+func (rb *RingBuffer[T]) Clear() {
+	rb.head = 0
+	rb.tail = 0
+	rb.count = 0
+}
+
+// EnsureCapacity increases the capacity of the buffer to at least the given capacity.
+// If the buffer is already at least that large, this is a no-op.
+func (rb *RingBuffer[T]) EnsureCapacity(minCapacity int) {
+	if minCapacity <= rb.capacity {
+		return
+	}
+	newBuf := make([]T, minCapacity)
+	if rb.count > 0 {
+		if rb.head < rb.tail {
+			copy(newBuf, rb.buf[rb.head:rb.tail])
+		} else {
+			n := copy(newBuf, rb.buf[rb.head:])
+			copy(newBuf[n:], rb.buf[:rb.tail])
+		}
+	}
+	rb.buf = newBuf
+	rb.head = 0
+	rb.tail = rb.count
+	rb.capacity = minCapacity
+}
+
+// ToSlice returns a slice of the items in the buffer (not aliased).
+func (rb *RingBuffer[T]) ToSlice() []T {
+	if rb.count == 0 {
+		return []T{}
+	}
+	out := make([]T, rb.count)
+	if rb.tail > rb.head {
+		copy(out, rb.buf[rb.head:rb.tail])
+	} else {
+		copy(out, rb.buf[rb.head:])
+		copy(out[rb.capacity-rb.head:], rb.buf[:rb.tail])
+	}
+	return out
+}
+
+func (rb *RingBuffer[T]) String() string {
+	return fmt.Sprintf("RingBuffer[%d/%d]", rb.count, rb.capacity)
+}

ring_buffer_test.go
@@ -1,0 +1,180 @@
+package zk
+
+import (
+	"testing"
+)
+
+func TestRingBuffer_Cap(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	if rb.Cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.Cap())
+	}
+}
+
+func TestRingBuffer_Push(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.Push(i)
+	}
+	if rb.Len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.Len())
+	}
+	if rb.Cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.Cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.ToSlice(), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+		t.Fatalf("expected items {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, got %v", rb.ToSlice())
+	}
+
+	// Overwrite items in the buffer.
+	for i := 0; i < 5; i++ {
+		rb.Push(-i)
+	}
+	if rb.Len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.Len())
+	}
+	if rb.Cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.Cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.ToSlice(), []int{5, 6, 7, 8, 9, 0, -1, -2, -3, -4}) {
+		t.Fatalf("expected items {5, 6, 7, 8, 0, 0, -1, -2, -3, -4}, got %v", rb.ToSlice())
+	}
+}
+
+func TestRingBuffer_Offer(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		if !rb.Offer(i) {
+			t.Fatalf("expected offer to succeed")
+		}
+	}
+	if rb.Len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.Len())
+	}
+	if rb.Cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.Cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.ToSlice(), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+		t.Fatalf("expected items {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, got %v", rb.ToSlice())
+	}
+
+	// Offer will refuse to overwrite items in a full buffer.
+	if rb.Offer(11) {
+		t.Fatalf("expected offer to fail")
+	}
+}
+
+func TestRingBuffer_Pop(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.Push(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		item, ok := rb.Pop()
+		if !ok {
+			t.Fatalf("expected item %d, got none", i)
+		}
+		if item != i {
+			t.Fatalf("expected item %d, got %d", i, item)
+		}
+	}
+
+	// Verify that the buffer is empty
+	if rb.Len() != 0 {
+		t.Fatalf("expected length 0, got %d", rb.Len())
+	}
+	_, ok := rb.Pop()
+	if ok {
+		t.Fatalf("expected no item, got one")
+	}
+}
+
+func TestRingBuffer_Peek(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.Push(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		item, ok := rb.Peek()
+		if !ok {
+			t.Fatalf("expected item %d, got none", i)
+		}
+		if item != i {
+			t.Fatalf("expected item %d, got %d", i, item)
+		}
+		_, _ = rb.Pop()
+	}
+
+	// Verify that the buffer is empty
+	if rb.Len() != 0 {
+		t.Fatalf("expected length 0, got %d", rb.Len())
+	}
+	_, ok := rb.Peek()
+	if ok {
+		t.Fatalf("expected no item, got one")
+	}
+}
+
+func TestRingBuffer_Clear(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.Push(i)
+	}
+
+	rb.Clear()
+	if rb.Len() != 0 {
+		t.Fatalf("expected length 0, got %d", rb.Len())
+	}
+}
+
+func TestRingBuffer_EnsureCapacity(t *testing.T) {
+	rb := NewRingBuffer[int](10)
+
+	for i := 0; i < 15; i++ {
+		rb.Push(i)
+	}
+
+	rb.EnsureCapacity(20)
+	if rb.Len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.Len())
+	}
+	if rb.Cap() != 20 {
+		t.Fatalf("expected capacity 20, got %d", rb.Cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.ToSlice(), []int{5, 6, 7, 8, 9, 10, 11, 12, 13, 14}) {
+		t.Fatalf("expected items {5, 6, 7, 8, 9, 10, 11, 12, 13, 14}, got %v", rb.ToSlice())
+	}
+
+	rb.EnsureCapacity(5) // should not change capacity
+	if rb.Cap() != 20 {
+		t.Fatalf("expected capacity 20, got %d", rb.Cap())
+	}
+}
+
+func slicesEqual[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}

soft-stech/go-zk

ring_buffer.go
@@ -1,0 +1,134 @@
+package zk
+
+import "fmt"
+
+// ringBuffer is a circular buffer that overwrites the oldest item when full.
+// It is not thread-safe.
+type ringBuffer[T any] struct {
+	// The buffer.
+	buf []T
+	// The index of the first item in the buffer.
+	head int
+	// The index of the last item in the buffer.
+	tail int
+	// The number of items in the buffer.
+	count int
+	// The capacity of the buffer.
+	capacity int
+}
+
+// newRingBuffer creates a new ringBuffer with the given capacity.
+func newRingBuffer[T any](capacity int) *ringBuffer[T] {
+	return &ringBuffer[T]{
+		buf:      make([]T, capacity),
+		capacity: capacity,
+	}
+}
+
+func (rb *ringBuffer[T]) isEmpty() bool {
+	return rb.count == 0
+}
+
+// len returns the number of items in the buffer.
+func (rb *ringBuffer[T]) len() int {
+	return rb.count
+}
+
+// cap returns the capacity of the buffer.
+func (rb *ringBuffer[T]) cap() int {
+	return rb.capacity
+}
+
+// offer adds an item to the buffer, if there is space.
+// Returns true if the item was added, false otherwise (buffer full).
+func (rb *ringBuffer[T]) offer(t T) bool {
+	if rb.count == rb.capacity {
+		return false
+	}
+	rb.buf[rb.tail] = t
+	rb.tail = (rb.tail + 1) % rb.capacity
+	rb.count++
+	return true
+}
+
+// push adds an item to the buffer.
+// If the buffer is full, the oldest item is overwritten.
+func (rb *ringBuffer[T]) push(t T) {
+	rb.buf[rb.tail] = t
+	rb.tail = (rb.tail + 1) % rb.capacity
+	if rb.count == rb.capacity {
+		rb.head = rb.tail
+	} else {
+		rb.count++
+	}
+}
+
+// peek returns the oldest item in the buffer, without removing it.
+// If the buffer is empty, returns the zero value and false.
+func (rb *ringBuffer[T]) peek() (T, bool) {
+	if rb.count == 0 {
+		var zero T
+		return zero, false
+	}
+	return rb.buf[rb.head], true
+}
+
+// pop returns the oldest item in the buffer, removing it.
+// If the buffer is empty, returns the zero value and false.
+func (rb *ringBuffer[T]) pop() (T, bool) {
+	if rb.count == 0 {
+		var zero T
+		return zero, false
+	}
+	t := rb.buf[rb.head]
+	rb.head = (rb.head + 1) % rb.capacity
+	rb.count--
+	return t, true
+}
+
+// clear removes all items from the buffer.
+func (rb *ringBuffer[T]) clear() {
+	rb.head = 0
+	rb.tail = 0
+	rb.count = 0
+}
+
+// ensureCapacity increases the capacity of the buffer to at least the given capacity.
+// If the buffer is already at least that large, this is a no-op.
+func (rb *ringBuffer[T]) ensureCapacity(minCapacity int) {
+	if minCapacity <= rb.capacity {
+		return
+	}
+	newBuf := make([]T, minCapacity)
+	if rb.count > 0 {
+		if rb.head < rb.tail {
+			copy(newBuf, rb.buf[rb.head:rb.tail])
+		} else {
+			n := copy(newBuf, rb.buf[rb.head:])
+			copy(newBuf[n:], rb.buf[:rb.tail])
+		}
+	}
+	rb.buf = newBuf
+	rb.head = 0
+	rb.tail = rb.count
+	rb.capacity = minCapacity
+}
+
+// toSlice returns a slice of the items in the buffer (not aliased).
+func (rb *ringBuffer[T]) toSlice() []T {
+	if rb.count == 0 {
+		return []T{}
+	}
+	out := make([]T, rb.count)
+	if rb.tail > rb.head {
+		copy(out, rb.buf[rb.head:rb.tail])
+	} else {
+		copy(out, rb.buf[rb.head:])
+		copy(out[rb.capacity-rb.head:], rb.buf[:rb.tail])
+	}
+	return out
+}
+
+func (rb *ringBuffer[T]) String() string {
+	return fmt.Sprintf("ringBuffer[%d/%d]", rb.count, rb.capacity)
+}

ring_buffer_test.go
@@ -1,0 +1,180 @@
+package zk
+
+import (
+	"testing"
+)
+
+func Test_ringBuffer_cap(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	if rb.cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.cap())
+	}
+}
+
+func Test_ringBuffer_push(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.push(i)
+	}
+	if rb.len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.len())
+	}
+	if rb.cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.toSlice(), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+		t.Fatalf("expected items {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, got %v", rb.toSlice())
+	}
+
+	// Overwrite items in the buffer.
+	for i := 0; i < 5; i++ {
+		rb.push(-i)
+	}
+	if rb.len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.len())
+	}
+	if rb.cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.toSlice(), []int{5, 6, 7, 8, 9, 0, -1, -2, -3, -4}) {
+		t.Fatalf("expected items {5, 6, 7, 8, 0, 0, -1, -2, -3, -4}, got %v", rb.toSlice())
+	}
+}
+
+func Test_ringBuffer_offer(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		if !rb.offer(i) {
+			t.Fatalf("expected offer to succeed")
+		}
+	}
+	if rb.len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.len())
+	}
+	if rb.cap() != 10 {
+		t.Fatalf("expected capacity 10, got %d", rb.cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.toSlice(), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+		t.Fatalf("expected items {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, got %v", rb.toSlice())
+	}
+
+	// offer will refuse to overwrite items in a full buffer.
+	if rb.offer(11) {
+		t.Fatalf("expected offer to fail")
+	}
+}
+
+func Test_ringBuffer_pop(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.push(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		item, ok := rb.pop()
+		if !ok {
+			t.Fatalf("expected item %d, got none", i)
+		}
+		if item != i {
+			t.Fatalf("expected item %d, got %d", i, item)
+		}
+	}
+
+	// Verify that the buffer is empty
+	if rb.len() != 0 {
+		t.Fatalf("expected length 0, got %d", rb.len())
+	}
+	_, ok := rb.pop()
+	if ok {
+		t.Fatalf("expected no item, got one")
+	}
+}
+
+func Test_ringBuffer_peek(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.push(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		item, ok := rb.peek()
+		if !ok {
+			t.Fatalf("expected item %d, got none", i)
+		}
+		if item != i {
+			t.Fatalf("expected item %d, got %d", i, item)
+		}
+		_, _ = rb.pop()
+	}
+
+	// Verify that the buffer is empty
+	if rb.len() != 0 {
+		t.Fatalf("expected length 0, got %d", rb.len())
+	}
+	_, ok := rb.peek()
+	if ok {
+		t.Fatalf("expected no item, got one")
+	}
+}
+
+func Test_ringBuffer_clear(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	for i := 0; i < 10; i++ {
+		rb.push(i)
+	}
+
+	rb.clear()
+	if rb.len() != 0 {
+		t.Fatalf("expected length 0, got %d", rb.len())
+	}
+}
+
+func Test_ringBuffer_ensureCapacity(t *testing.T) {
+	rb := newRingBuffer[int](10)
+
+	for i := 0; i < 15; i++ {
+		rb.push(i)
+	}
+
+	rb.ensureCapacity(20)
+	if rb.len() != 10 {
+		t.Fatalf("expected length 10, got %d", rb.len())
+	}
+	if rb.cap() != 20 {
+		t.Fatalf("expected capacity 20, got %d", rb.cap())
+	}
+
+	// Verify the contents of the buffer
+	if !slicesEqual(rb.toSlice(), []int{5, 6, 7, 8, 9, 10, 11, 12, 13, 14}) {
+		t.Fatalf("expected items {5, 6, 7, 8, 9, 10, 11, 12, 13, 14}, got %v", rb.toSlice())
+	}
+
+	rb.ensureCapacity(5) // should not change capacity
+	if rb.cap() != 20 {
+		t.Fatalf("expected capacity 20, got %d", rb.cap())
+	}
+}
+
+func slicesEqual[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}